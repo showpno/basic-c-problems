6bd8254b0ee6e886c226d11e1e52926f1a19ffcd
Rafiu Islam <rafiuisalm302@gmail.com>
2023-12-13 15:20:14 +0600

    done

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float divide(float x, float y)
+{
+float result=x/y;
+    printf("The division is = %.2f\n",result);
+return 0;
+}
+int main()
+{
+    divide(5,2);
+    divide(1110,187);
+    divide(11110,189);
+}
